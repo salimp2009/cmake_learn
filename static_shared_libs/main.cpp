c8d304d29980278b49d76cc52ed3097fbab4caaa
salimp2009@gmail.com <salimp2009@gmail.com>
2023-10-11 21:45:31 +0300

    minor changes to test Cmake

--- static_shared_libs/main.cpp
+++ static_shared_libs/main.cpp
@@ -1,9 +1,12 @@
 #include "message.hpp"
 #include <cstdlib>
 #include <iostream>
+#include <memory>
 
 int main() {
   Message msg("Hello cmake");
-  std::cout << msg << '\n';
+  const auto sptr = std::make_shared<int>(5);
+  auto sptr2 = sptr;
+  std::cout << msg << " ptr: " << *sptr << ", ptr2: " << *sptr2 << '\n';
   return 0;
 }
