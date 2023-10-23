809195c44c6cc93d45110f9b2ef50fe030e9c375
salimp2009@gmail.com <salimp2009@gmail.com>
2023-10-23 17:13:26 +0300

    added findpackage to test using the sorted project that installs sort print libraries but somehow could not make it work

--- lib/sorted/sorted-config-debug.cmake
+++ lib/sorted/sorted-config-debug.cmake
@@ -5,25 +5,25 @@
 # Commands may need to know the format version.
 set(CMAKE_IMPORT_FILE_VERSION 1)
 
-# Import target "sorted::sort" for configuration "Debug"
-set_property(TARGET sorted::sort APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
-set_target_properties(sorted::sort PROPERTIES
+# Import target "sort" for configuration "Debug"
+set_property(TARGET sort APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(sort PROPERTIES
   IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
   IMPORTED_LOCATION_DEBUG "/home/salim/repos/cpp_sandbox/cmake_learn/lib/sorted/libsort.a"
   )
 
-list(APPEND _cmake_import_check_targets sorted::sort )
-list(APPEND _cmake_import_check_files_for_sorted::sort "/home/salim/repos/cpp_sandbox/cmake_learn/lib/sorted/libsort.a" )
+list(APPEND _cmake_import_check_targets sort )
+list(APPEND _cmake_import_check_files_for_sort "/home/salim/repos/cpp_sandbox/cmake_learn/lib/sorted/libsort.a" )
 
-# Import target "sorted::print" for configuration "Debug"
-set_property(TARGET sorted::print APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
-set_target_properties(sorted::print PROPERTIES
+# Import target "print" for configuration "Debug"
+set_property(TARGET print APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(print PROPERTIES
   IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
   IMPORTED_LOCATION_DEBUG "/home/salim/repos/cpp_sandbox/cmake_learn/lib/sorted/libprint.a"
   )
 
-list(APPEND _cmake_import_check_targets sorted::print )
-list(APPEND _cmake_import_check_files_for_sorted::print "/home/salim/repos/cpp_sandbox/cmake_learn/lib/sorted/libprint.a" )
+list(APPEND _cmake_import_check_targets print )
+list(APPEND _cmake_import_check_files_for_print "/home/salim/repos/cpp_sandbox/cmake_learn/lib/sorted/libprint.a" )
 
 # Commands beyond this point should not need to know the version.
 set(CMAKE_IMPORT_FILE_VERSION)
---
+++ use_findpackage/main.cpp
@@ -0,0 +1,4 @@
+#include <vector>
+// #include <print>
+// #include <sort>
+int main() { [[maybe_unused]] std::vector v = {2, 3, 1, 7, 5, 8, 9, 7, 0}; }
