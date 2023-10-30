fb30e2a99c01c67fdf60b903788541bb35735fd1
salimp2009@gmail.com <salimp2009@gmail.com>
2023-10-30 13:12:59 +0300

    removing unintended by products of install command

--- include/sorted/include/print/print.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef PRINT_PRINT_HPP
-#define PRINT_PRINT_HPP
-#include <vector>
-
-void myPrint(std::vector<double> const &list);
-#endif // PRINT_PRINT_HPP
--- include/sorted/include/sort/sort.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef SORT_SORT_HPP
-#define SORT_SORT_HPP
-
-#include <vector>
-
-void mySort(std::vector<double> &list);
-
-#endif // SORT_SORT_HPP
