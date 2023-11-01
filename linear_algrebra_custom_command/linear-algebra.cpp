6731571958af9261a3a80bd48e48519d74284eb7
salimp2009@gmail.com <salimp2009@gmail.com>
2023-11-01 19:18:02 +0300

    added add_custom_command and add_custom_target ; custom_command version uses some older version CXXBLAS CXXLAPACK therefore gives an error since the BLAS & CBLAS is part of LAPACK during build time

--- linear_algrebra_custom_command/linear-algebra.cpp
+++ linear_algrebra_custom_command/linear-algebra.cpp
@@ -1,11 +1,9 @@
-#include <chrono>
-#include <cmath>
-#include <cstdlib>
-#include <iomanip>
-#include <iostream>
-#include <vector>
+#include "CxxBLAS.hpp"
+#include "CxxLAPACK.hpp"
 
-#include <Eigen/Dense>
+#include <iostream>
+#include <random>
+#include <vector>
 
 int main(int argc, char **argv) {
   if (argc != 2) {
@@ -13,51 +11,45 @@ int main(int argc, char **argv) {
     return EXIT_FAILURE;
   }
 
-  std::chrono::time_point<std::chrono::system_clock> start, end;
-  std::chrono::duration<double> elapsed_seconds;
-  std::time_t end_time;
-
-  std::cout << "Number of threads used by Eigen: " << Eigen::nbThreads()
-            << std::endl;
+  // Generate a uniform distribution of real number between -1.0 and 1.0
+  std::random_device rd;
+  std::mt19937 mt(rd());
+  std::uniform_real_distribution<double> dist(-1.0, 1.0);
 
   // Allocate matrices and right-hand side vector
-  start = std::chrono::system_clock::now();
   int dim = std::atoi(argv[1]);
-  Eigen::MatrixXd A = Eigen::MatrixXd::Random(dim, dim);
-  Eigen::VectorXd b = Eigen::VectorXd::Random(dim);
-  end = std::chrono::system_clock::now();
+  std::vector<double> A(dim * dim);
+  std::vector<double> b(dim);
+  std::vector<int> ipiv(dim);
+  // Fill matrix and RHS with random numbers between -1.0 and 1.0
+  for (int r = 0; r < dim; r++) {
+    for (int c = 0; c < dim; c++) {
+      A[r + c * dim] = dist(mt);
+    }
+    b[r] = dist(mt);
+  }
 
-  // Report times
-  elapsed_seconds = end - start;
-  end_time = std::chrono::system_clock::to_time_t(end);
-  std::cout << "matrices allocated and initialized "
-            << std::put_time(std::localtime(&end_time), "%a %b %d %Y %r\n")
-            << "elapsed time: " << elapsed_seconds.count() << "s\n";
+  // Scale RHS vector by a random number between -1.0 and 1.0
+  C_DSCAL(dim, dist(mt), b.data(), 1);
+  std::cout << "C_DSCAL done" << std::endl;
 
-  start = std::chrono::system_clock::now();
   // Save matrix and RHS
-  Eigen::MatrixXd A1 = A;
-  Eigen::VectorXd b1 = b;
-  end = std::chrono::system_clock::now();
-  end_time = std::chrono::system_clock::to_time_t(end);
-  std::cout << "Scaling done, A and b saved "
-            << std::put_time(std::localtime(&end_time), "%a %b %d %Y %r\n")
-            << "elapsed time: " << elapsed_seconds.count() << "s\n";
+  std::vector<double> A1(A);
+  std::vector<double> b1(b);
 
-  start = std::chrono::system_clock::now();
-  Eigen::VectorXd x = A.lu().solve(b);
-  end = std::chrono::system_clock::now();
+  int info;
+  info = C_DGESV(dim, 1, A.data(), dim, ipiv.data(), b.data(), dim);
+  std::cout << "C_DGESV done" << std::endl;
+  std::cout << "info is " << info << std::endl;
 
-  // Report times
-  elapsed_seconds = end - start;
-  end_time = std::chrono::system_clock::to_time_t(end);
-
-  double relative_error = (A * x - b).norm() / b.norm();
-
-  std::cout << "Linear system solver done "
-            << std::put_time(std::localtime(&end_time), "%a %b %d %Y %r\n")
-            << "elapsed time: " << elapsed_seconds.count() << "s\n";
-  std::cout << "relative error is " << relative_error << std::endl;
+  double eps = 0.0;
+  for (int i = 0; i < dim; ++i) {
+    double sum = 0.0;
+    for (int j = 0; j < dim; ++j)
+      sum += A1[i + j * dim] * b[j];
+    eps += std::abs(b1[i] - sum);
+  }
+  std::cout << "check is " << eps << std::endl;
 
   return 0;
 }
