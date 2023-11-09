3d22d52f130195bf0969a72758cd1defc4edbf48
salimp2009@gmail.com <salimp2009@gmail.com>
2023-11-09 22:41:59 +0300

    added color for message and included as a cmake and macro

--- project_struct_funct_macro/cmake/add_catch_test.cmake
+++ project_struct_funct_macro/cmake/add_catch_test.cmake
@@ -2,14 +2,25 @@ set(num_macro_calls
     0
     PARENT_SCOPE)
 
+# list(APPEND CMAKE_MODULE_PATH "${PROJECT_SOURCE_DIR}/cmake") include(colors)
+# define_colors()
+
+# the above color cmake and module_path variables are defined at top level so
+# all other modules can use it
+
 macro(add_catch_test _name _cost)
   math(EXPR num_macro_calls "${num_macro_calls}+1")
-  message(STATUS "add_catch_test called with ${ARGC} arguments: ${ARGV}")
+  message(
+    STATUS
+      "${Green}add_catch_test called with  ${ARGC} arguments: ${ARGV}${ColourReset}"
+  )
 
   set(_argn "${ARGN}")
   if(_argn)
     message(
-      STATUS "oops - macro received argument(s) we did not expect: ${ARGN}")
+      STATUS
+        "${Yellow}oops - macro received argument(s) we did not expect:${ARGN}${ColourReset}"
+    )
   endif()
 
   add_test(
--- project_struct_funct_macro/src/main.cpp
+++ project_struct_funct_macro/src/main.cpp
@@ -1,12 +1,17 @@
 #include "sum_integers.hpp"
 #include <algorithm>
+#include <cstddef>
+#include <cstdio>
+#include <experimental/propagate_const>
+#include <iomanip>
+#include <ios>
 #include <iostream>
+#include <span>
 #include <string>
 #include <vector>
 // we assume all arguments are integers and we sum them up
 // for simplicity we do not verify the type of arguments
 int main(int argc, const char *argv[]) {
-
   std::vector<int> integers;
   std::ranges::transform(&argv[1], &argv[argc], std::back_inserter(integers),
                          [](const auto &num) { return std::stoi(num); });
@@ -18,4 +23,14 @@ int main(int argc, const char *argv[]) {
   const auto sum = sum_integers(integers);
 
   std::cout << "sum of integers:" << sum << '\n';
+
+  float data[1]{3.141592};
+  auto const const_bytes = std::as_bytes(std::span{data});
+  auto const writable_bytes = std::as_writable_bytes(std::span{data});
+  writable_bytes[3] |= std::byte{0B1000'0000};
+  for (auto const b : const_bytes) {
+    std::cout << std::hex << std::uppercase << std::setfill('0') << std::setw(2)
+              << std::to_integer<int>(b) << ' ';
+  }
+  std::cout << std::dec << '\n';
 }
