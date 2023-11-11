5ba9f0237dde6f02f4983626417f15c5d461af41
salimp2009@gmail.com <salimp2009@gmail.com>
2023-11-11 20:33:25 +0300

    changed dependency graph format from png to svg in utility.cmake

--- project_struct_funct_macro/cmake/utility.cmake
+++ project_struct_funct_macro/cmake/utility.cmake
@@ -8,18 +8,21 @@ macro(generate_dep_graph)
   endif()
 
   set(DOT_FILE "dep_graph.dot")
-  set(PNG_FILE "${CMAKE_SOURCE_DIR}/docs/dep_graph.png")
+  # set(PNG_FILE "${CMAKE_SOURCE_DIR}/docs/dep_graph.png")
+  set(SVG_FILE "${CMAKE_SOURCE_DIR}/docs/dep_graph.svg")
 
   add_custom_target(
     dep_graph
     WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
-    DEPENDS ${PNG_FILE}
+    # DEPENDS ${PNG_FILE}
+    DEPENDS ${SVG_FILE}
     COMMENT "Creates a new dependency graph for the documentation"
     VERBATIM)
 
   add_custom_command(
-    OUTPUT ${PNG_FILE}
-    COMMAND ${DOT_EXECUTABLE} -T png ${DOT_FILE} -o ${PNG_FILE}
+    # OUTPUT ${PNG_FILE}
+    OUTPUT ${SVG_FILE}
+    COMMAND ${DOT_EXECUTABLE} -T png ${DOT_FILE} -o ${SVG_FILE}
     WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
     DEPENDS ${DOT_FILE}
     COMMENT "Creates a new dependency graph as png")
@@ -28,7 +31,7 @@ macro(generate_dep_graph)
     OUTPUT ${DOT_FILE}
     COMMAND ${CMAKE_COMMAND} --graphviz=${DOT_FILE} "."
     WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
-    # DEPENDS ${PROJECT_NAME}
+    # DEPENDS sum_integers
     COMMENT "Creates a new dependency graph")
 
 endmacro()
