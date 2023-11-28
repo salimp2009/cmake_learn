8c16ec36e6ed4e5138914b4233ecf8739ef6c600
salimp2009@gmail.com <salimp2009@gmail.com>
2023-11-28 17:35:47 +0300

    continue testing

--- install_project_package/src/message/Message.cpp
+++ install_project_package/src/message/Message.cpp
@@ -14,9 +14,8 @@ std::ostream &operator<<(std::ostream &os, const Message &obj) {
      << "git_hash: " << GIT_HASH << " version: " << PROJECT_VERSION << '\n'
      << "git status " << GIT_STATUS << '\n'
      << "version major: " << PROJECT_VERSION_MAJOR
-     << " minor: " << PROJECT_VERSION_MINOR
-     << '\n'
-     // << " patch: " << PROJECT_VERSION_PATCH << '\n'
+     << " minor: " << PROJECT_VERSION_MINOR << '\n'
+     << " patch: " << PROJECT_VERSION_PATCH << '\n'
      << "...and here is its UUID: " << getUUID() << '\n';
   return os;
 }
