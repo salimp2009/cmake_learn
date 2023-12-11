d2ddcf123ae28f25939b6d1f2b847226d00ed3c7
salimp2009@gmail.com <salimp2009@gmail.com>
2023-12-11 15:30:00 +0300

    started adding a optional storage
    - basically opt_storage is a struct with an struct Empty and union that has either Empty or type T m_value
    and has bool has_value
    - default constructor has Empty and has_value false
    - there is a also variadic templated constructor that take Args
    - also made derived class to add some operations to avoid a big class
    - still in progress

--- install_project_package/include/concepts/conceptmoptional.hpp
+++ install_project_package/include/concepts/conceptmoptional.hpp
@@ -6,6 +6,7 @@
 #include <memory>
 #include <optional>
 #include <type_traits>
+#include <utility>
 namespace sp {
 
 struct Empty {};
@@ -27,8 +28,58 @@ template <typename T> union storage_t {
   constexpr storage_t() = default;
 
   // T *as() { std::construct_at(std::bit_cast<T *>(data), T()); }
-  T *as() { return std::bit_cast<T *>(new (std::addressof(data)) T()); }
+  [[nodiscard("returned pointer not used!!")]] T *as() {
+    return std::bit_cast<T *>(new (std::addressof(data)) T());
+  }
 };
+
+namespace detail {
+
+template <typename T, bool = std::is_trivially_destructible_v<T>>
+struct opt_storage_base {
+  constexpr opt_storage_base() noexcept : m_empty{}, m_has_value{false} {}
+
+  template <typename... U>
+  constexpr opt_storage_base(std::in_place_t, U &&...args) noexcept
+      : m_value{std::forward<U>(args)...}, m_has_value{true} {}
+
+  ~opt_storage_base()
+    requires(not std::is_trivially_destructible_v<T>)
+  {
+    if (m_has_value) {
+      m_value.~T();
+      m_has_value = false;
+    }
+  }
+
+  struct empty {};
+  union {
+    empty m_empty{};
+    T m_value;
+  };
+  bool m_has_value{};
+};
+
+template <typename T> struct opt_operations_base : opt_storage_base<T> {
+  using opt_storage_base<T>::opt_storage_base;
+
+  void hard_reset() noexcept { get().~T(); }
+
+  template <typename... Args> void construct(Args... args) {
+    // if C++20
+    std::construct_at(std::addressof(this->m_value), args...);
+    // if not C++20
+    // ::new (std::addressof(this->value)) T(std::forward<Args>(args)...);
+  }
+
+  bool has_value() const { return this->m_has_value; }
+
+  constexpr T &get() & { return this->m_value; }
+  constexpr const T &get() const & { return this->m_value; }
+  constexpr T &&get() && { return std::move(this->m_value); }
+};
+
+} // end of namespace detail
 
 template <typename T> class message_EXPORT moptional {
 public:
@@ -72,11 +123,25 @@ private:
 template <typename T> struct moptional2 {
 public:
   moptional2() = default;
+
   moptional2(const moptional2 &other)
     requires(std::is_copy_constructible_v<T>)
       : value{other.value} {}
 
+  ~moptional2()
+    requires(not std::is_trivially_destructible_v<T>)
+  {
+    if (has_value) {
+      value.as()->T();
+    }
+  }
+
+  ~moptional2() = default;
+
 private:
   storage_t<T> value{};
+
+  bool has_value{};
 };
+
 } // namespace sp
