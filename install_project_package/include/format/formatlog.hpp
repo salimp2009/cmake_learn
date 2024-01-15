0d0ab746df4f07f1c71b98f2661124bdad23bf06
salimp2009@gmail.com <salimp2009@gmail.com>
2024-01-15 21:10:31 +0300

    added a helper vlogger function and make_braces helper function
    - will implement the generation of braces depending on the number of
    args instead of user defining specifiers & braces

--- install_project_package/include/format/formatlog.hpp
+++ install_project_package/include/format/formatlog.hpp
@@ -1,9 +1,13 @@
 #pragma once
 
+#include <algorithm>
+#include <array>
 #include <format>
 #include <iostream>
 #include <messageExport.h>
 #include <source_location>
+#include <string_view>
+#include <strings.h>
 #include <utility>
 
 namespace sp {
@@ -13,40 +17,60 @@ enum class message_EXPORT LogLevel { Info, Warning, Error };
 } // namespace sp
 
 template <> struct std::formatter<sp::LogLevel> : std::formatter<const char *> {
+
   constexpr static const char *LEVEL_NAMES[] = {"Info", "Warning", "Error"};
+
   auto format(sp::LogLevel c, format_context &ctx) const {
     using enum sp::LogLevel;
     return std::formatter<const char *>::format(
         LEVEL_NAMES[std::to_underlying(c)], ctx);
-    // return std::format_to(ctx.out(), "{}",
-    // LEVEL_NAMES[std::to_underlying(c)]);
   }
 };
 
 namespace sp {
+
+template <std::size_t Args> constexpr auto make_braces() {
+  constexpr std::array<char, 4> brace{"{} "};
+  // size braces without '\0'
+  constexpr auto brace_size = brace.size() - 1;
+  // 2 chars for newline & '\0'
+  constexpr auto offset = 2u;
+
+  std::array<char, Args * brace_size + offset> braces{};
+  constexpr auto bracesLength = brace_size - offset;
+
+  auto indx = 0;
+  std::for_each_n(braces.begin(), bracesLength, [&](auto &elem) {
+    elem = brace[indx % brace_size];
+    ++indx;
+  });
+  braces[bracesLength] = '\0';
+  return braces;
+}
+
+void vlogger(LogLevel level, std::string_view fmt, std::format_args &&args);
+
 template <typename... Args>
-void logger(LogLevel level, std::string_view fmt, Args &&...args) {
+constexpr void logger(LogLevel level, std::string_view fmt, Args &&...args) {
   using namespace std::string_view_literals;
 
-  std::clog << std::format("LOG LEVEL:{}, "sv, level)
-            << std::vformat(fmt, std::make_format_args(args...)) << '\n';
-};
+  vlogger(level, fmt, std::make_format_args(args...));
+  // std::clog << std::format("LOG LEVEL:{}, "sv, level)
+  //           << std::vformat(fmt, std::make_format_args(args...)) << '\n';
+}
 
 struct message_EXPORT Logger {
 
   template <typename... Args>
-  constexpr void operator()(
-      LogLevel level, std::string_view fmt, Args &&...args
-      /* std::source_location loc = std::source_location::current() */) const {
+  constexpr void operator()(LogLevel level, std::string_view fmt,
+                            Args &&...args) const {
     using namespace std::string_view_literals;
-    std::source_location loc = std::source_location::current();
-    std::clog << std::format("LOG LEVEL:{}, location: {} "sv, level,
-                             loc.file_name())
+    std::clog << std::format("LOG LEVEL:{},  "sv, level)
               << std::vformat(fmt, std::make_format_args(args...)) << '\n';
   }
 };
 
 // template <typename... Args> Logger(Args &&...) -> Logger<Args...>;
 
-inline constexpr Logger log{};
+message_EXPORT inline constexpr Logger log{};
 } // namespace sp
--- install_project_package/src/format/formatlog.cpp
+++ install_project_package/src/format/formatlog.cpp
@@ -1,7 +1,16 @@
 #include "formatlog.hpp"
+#include <ctime>
+#include <source_location>
 // #include <iostream>
 
 namespace sp {
+
+void vlogger(LogLevel level, std::string_view fmt, std::format_args &&args) {
+  using namespace std::string_view_literals;
+
+  std::clog << std::format("LOG LEVEL: {} "sv, level) << std::vformat(fmt, args)
+            << '\n';
+};
 
 // void logger(LogLevel level, std::string_view fmt, const auto &...args) {
 //   // auto loc = std::source_location::current();
