014b4edbf383fec95f8e3e61553698d6679abeae
salimp2009@gmail.com <salimp2009@gmail.com>
2024-03-27 00:30:03 +0300

    added some Qt headers to see if it is working :)

--- qtsample/main.cpp
+++ qtsample/main.cpp
@@ -1,10 +1,14 @@
-// #include <QtGui/QAccessible>
+#include <QCommandLineParser>
 #include <cstdio>
 #include <qapplicationstatic.h>
+#include <qcoreapplication.h>
+#include <qcoreevent.h>
 #include <qdatetime.h>
+#include <qwindowdefs.h>
 
 auto main(int argc, char *argv[]) -> int {
   QCoreApplication app(argc, argv);
+  QCommandLineParser parser;
   // using namespace std::string_view_literals;
   QDate date{2024, 3, 20};
   std::printf("Hello Qt day: %d month: %d", date.day(), date.month());
